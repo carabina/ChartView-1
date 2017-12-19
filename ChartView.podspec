143acc26bdf87e1d6569a0e3f752da8621cfb122
liuqing520it <330663384@qq.com>
2017-12-19 12:52:05 +0800

    小修改

--- ChartView.podspec
+++ ChartView.podspec
@@ -2,11 +2,10 @@ Pod::Spec.new do |s|
 s.name     = 'ChartView'
 s.version  = '1.1.0'
 s.platform = :ios, '9.0'
-s.license  = 'MIT'
 s.summary  = 'show performance chart view'
 s.homepage = 'https://github.com/liuqing520it/ChartView'
 s.author   = { "liuqing520it" => "330663384@qq.com" }
-s.source   = { :git => 'https://github.com/liuqing520it/ChartView.git', :tag => s.version }
+s.source   = { :git => 'https://github.com/liuqing520it/ChartView.git', :tag => s.version}
 
 s.description = '一款简单的图表视图折线图或曲线图,每条竖线都可点击切换每天的数据,用来展示店员业绩;'
 s.source_files = 'ChartView/*.swift'
--- ChartView/LQChartHeaderTitle.swift
+++ ChartView/LQChartHeaderTitle.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Foundation
 
 class LQChartHeaderTitle: UIView {
 
@@ -23,8 +24,8 @@ class LQChartHeaderTitle: UIView {
         let textString = String(dateString.suffix(5)) + "(\(weekString))" + "销售额:" + "￥" + saleCount
         let attribute = NSMutableAttributedString(string: textString)
         ///最后两位 金额颜色半透明
-        attribute.addAttributes([NSAttributedStringKey.foregroundColor : UIColor.init(white: 1.0, alpha: 0.4)], range: NSMakeRange((textString as NSString).length - 2, 2))
-        attribute.addAttributes([NSAttributedStringKey.foregroundColor : UIColor.init(white: 1.0, alpha: 1.0)], range: NSMakeRange(0, (textString as NSString).length - 2))
+        attribute.addAttributes([.foregroundColor : UIColor.init(white: 1.0, alpha: 0.4)], range: NSMakeRange((textString as NSString).length - 2, 2))
+        attribute.addAttributes([.foregroundColor : UIColor.init(white: 1.0, alpha: 1.0)], range: NSMakeRange(0, (textString as NSString).length - 2))
         titleLabel.attributedText = attribute
         titleLabel.sizeToFit()
     }
